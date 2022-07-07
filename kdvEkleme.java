982ca94b638688eb3d56fd00042ff9f509519e0d
Emre İşlek <95578438+islek-emre@users.noreply.github.com>
2022-07-07 11:44:18 +0300

    kdvEkleme
    Kullanıcıdan alınan fiyata %18 KDV ekleyerek sonucu geri döndürür.

---
+++ kdvEkleme.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class kdvEkleme {
+    public static void main(String[] args) {
+
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Ürünün KDV'siz fiyatını giriniz: ");
+        int fiyat = scan.nextInt();
+
+        double yeni_fiyat = fiyat + (fiyat * 0.18);
+
+        System.out.println("KDV'li fiyat = " + yeni_fiyat);
+    }
+}
